b777caca75a4aeb6c4632cf835b31c3f4241714f
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-19 11:07:29 +0600

    max seat select done

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,11 @@ const seatInfo = document.getElementById('seatInfo')
 const seatClass = 'Economy';
 let perSeatPrice = 550;
 let totalPrice = 0;
-
+let selectedTotalSeat = 0;
+let maxSelect = 0;
 const perSeattotal = document.getElementById('perSeatTotal');
 
+const grandTotalPrice = document.getElementById('grandTotal');
 
 
 const allBtns = document.getElementsByClassName('seat_btn');
@@ -21,29 +23,33 @@ for (const button of allBtns) {
     // const seatNumber = button.innerText;
 
     button.addEventListener('click', function () {
-        button.style.backgroundColor = 'rgb(34 197 94)';
-        button.style.color = 'white'
-        seatsLeft--;
-        selectedSeat++;
+        if (maxSelect < 4) {
+            maxSelect++;
+            button.style.backgroundColor = 'rgb(34 197 94)';
+            button.style.color = 'white'
+            seatsLeft--;
+            selectedSeat++;
 
-        totalPrice += perSeatPrice;
-        perSeattotal.innerText = totalPrice;
+            totalPrice += perSeatPrice;
+            perSeattotal.innerText = totalPrice;
 
-        seatsLeftDisplay.innerText = seatsLeft;
-        seatSelected.innerText = selectedSeat;
-        seatSelected.style.backgroundColor = 'rgb(34 197 94)';
-        seatSelected.style.color = 'white';
-        seatSelected.style.padding = '4px';
-        seatSelected.style.borderRadius = '10px'
+            seatsLeftDisplay.innerText = seatsLeft;
+            seatSelected.innerText = selectedSeat;
+            seatSelected.style.backgroundColor = 'rgb(34 197 94)';
+            seatSelected.style.color = 'white';
+            seatSelected.style.padding = '4px';
+            seatSelected.style.borderRadius = '10px'
 
 
-        const selectedSeatsDetails = document.createElement('tr');
-        selectedSeatsDetails.innerHTML = `
+            const selectedSeatsDetails = document.createElement('tr');
+            selectedSeatsDetails.innerHTML = `
             <td>${button.innerText}</td>
             <td>${seatClass}</td>
             <td>${perSeatPrice}</td>
             
         `
-        seatInfo.append(selectedSeatsDetails)
+            seatInfo.append(selectedSeatsDetails)
+        }
     })
+
 }
