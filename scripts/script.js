53d782f7d22783fbe0a76fbffa5d0b40df575122
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-19 10:11:04 +0600

    appended seats info

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,9 @@ const seatSelected = document.getElementById('totalSeatSelected');
 let selectedSeat = 0;
 
 // appending the seats info
-const seatInfo = document.getElementById('')
+const seatInfo = document.getElementById('seatInfo')
+const seatClass = 'Economy';
+let perSeatPrice = 550;
 
 
 
@@ -17,10 +19,23 @@ for (const button of allBtns) {
 
     button.addEventListener('click', function () {
         button.style.backgroundColor= 'rgb(34 197 94)';
-            button.style.color='white'
+        button.style.color='white'
         seatsLeft --;
         selectedSeat++;
         seatsLeftDisplay.innerText = seatsLeft;
         seatSelected.innerText = selectedSeat;
+        seatSelected.style.backgroundColor = 'rgb(34 197 94)';
+        seatSelected.style.color = 'white';
+        seatSelected.style.padding = '4px';
+        seatSelected.style.borderRadius = '10px'
+
+        const selectedSeatsDetails = document.createElement('tr');
+        selectedSeatsDetails.innerHTML= `
+            <td>${button.innerText}</td>
+            <td>${seatClass}</td>
+            <td>${perSeatPrice}</td>
+            
+        `
+        seatInfo.append(selectedSeatsDetails)
     })
 }
