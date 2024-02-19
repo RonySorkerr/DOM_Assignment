4d9daf9b7a6fe2c893f06b63d05be10df92b548c
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-19 09:52:09 +0600

    adding features

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,26 @@
-const seatsLeftDisplay = document.getElementById('seatsLeft').innerText;
+let seatsLeft = 40;
+
+const seatsLeftDisplay = document.getElementById('seatsLeft');
+
+const seatSelected = document.getElementById('totalSeatSelected');
+let selectedSeat = 0;
+
+// appending the seats info
+const seatInfo = document.getElementById('')
+
+
 
 const allBtns = document.getElementsByClassName('seat_btn');
 
-for(const eachButton of allBtns){
-    const seatNumber = eachButton.innerText;
-    
-    eachButton.addEventListener('')
+for (const button of allBtns) {
+    // const seatNumber = button.innerText;
+
+    button.addEventListener('click', function () {
+        button.style.backgroundColor= 'rgb(34 197 94)';
+            button.style.color='white'
+        seatsLeft --;
+        selectedSeat++;
+        seatsLeftDisplay.innerText = seatsLeft;
+        seatSelected.innerText = selectedSeat;
+    })
 }
