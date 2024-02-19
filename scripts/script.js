97a53e26e6347a772bc6adec24762c6d81c06847
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-19 10:25:57 +0600

    seat selection done

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,9 @@ let selectedSeat = 0;
 const seatInfo = document.getElementById('seatInfo')
 const seatClass = 'Economy';
 let perSeatPrice = 550;
+let perSeatTotal = 0;
+
+const perSeattotal = document.getElementById('perSeatTotal');
 
 
 
@@ -18,19 +21,21 @@ for (const button of allBtns) {
     // const seatNumber = button.innerText;
 
     button.addEventListener('click', function () {
-        button.style.backgroundColor= 'rgb(34 197 94)';
-        button.style.color='white'
-        seatsLeft --;
+        button.style.backgroundColor = 'rgb(34 197 94)';
+        button.style.color = 'white'
+        seatsLeft--;
         selectedSeat++;
-        seatsLeftDisplay.innerText = seatsLeft;
+        perSeatPrice +
+            seatsLeftDisplay.innerText = seatsLeft;
         seatSelected.innerText = selectedSeat;
         seatSelected.style.backgroundColor = 'rgb(34 197 94)';
         seatSelected.style.color = 'white';
         seatSelected.style.padding = '4px';
         seatSelected.style.borderRadius = '10px'
 
+
         const selectedSeatsDetails = document.createElement('tr');
-        selectedSeatsDetails.innerHTML= `
+        selectedSeatsDetails.innerHTML = `
             <td>${button.innerText}</td>
             <td>${seatClass}</td>
             <td>${perSeatPrice}</td>
