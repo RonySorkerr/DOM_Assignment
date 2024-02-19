ec2ea956bc4d0a28e01be01ffbd9f69e903cc81e
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-19 17:25:27 +0600

    check the color

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,8 @@ let seatsLeft = 40;
 const seatsLeftDisplay = document.getElementById('seatsLeft');
 const seatSelected = document.getElementById('totalSeatSelected');
 const numberField = document.getElementById('numberInput')
-
 const couponApplyNotif = document.getElementById('maxSeatNot');
 const message = couponApplyNotif.parentElement;
-
 let selectedSeat = 0;
 // appending the seats info
 const seatInfo = document.getElementById('seatInfo')
@@ -13,20 +11,12 @@ const seatClass = 'Economy';
 let perSeatPrice = 550;
 let totalPrice = 0;
 let selectedTotalSeat = 0;
-
 let maxSelect = 0;
 const couponInput = document.getElementById('couponInput');
-
-
 const perSeattotal = document.getElementById('perSeatTotal');
-
 const grandTotalDisplay = document.getElementById('grandTotal');
-
 const allBtns = document.getElementsByClassName('seat_btn');
-
 for (const button of allBtns) {
-    // const seatNumber = button.innerText;
-
     button.addEventListener('click', function () {
         if (maxSelect < 4) {
             maxSelect++;
@@ -34,31 +24,23 @@ for (const button of allBtns) {
             button.style.color = 'white'
             seatsLeft--;
             selectedSeat++;
-
             button.disabled = true;
-
             totalPrice += perSeatPrice;
             perSeattotal.innerText = totalPrice;
-
             seatsLeftDisplay.innerText = seatsLeft;
             seatSelected.innerText = selectedSeat;
             seatSelected.style.backgroundColor = 'rgb(34 197 94)';
             seatSelected.style.color = 'white';
             seatSelected.style.padding = '4px';
             seatSelected.style.borderRadius = '10px'
-
-
             const selectedSeatsDetails = document.createElement('tr');
             selectedSeatsDetails.innerHTML = `
             <td>${button.innerText}</td>
             <td>${seatClass}</td>
-            <td>${perSeatPrice}</td>            
-            `
+            <td>${perSeatPrice}</td>`
             seatInfo.append(selectedSeatsDetails)
             grandTotalDisplay.innerText = totalPrice;
-
             numberField.disabled = false;
-
             if (maxSelect === 4) {
                 message.remove()
                 couponApplyBtn.disabled = false;
@@ -69,13 +51,10 @@ for (const button of allBtns) {
     })
 };
 
-
 const couponApplyBtn = document.getElementById('couponApply');
 const coupon15 = 'NEW15';
 const coupon20 = 'Couple 20';
-
 const grandColor = document.getElementById('grandColor');
-
 couponApplyBtn.addEventListener('click', function () {
     const couponValue = couponInput.value;
     if (couponValue === coupon15 || couponValue === coupon20) {
@@ -85,11 +64,8 @@ couponApplyBtn.addEventListener('click', function () {
         couponApplyBtn.parentElement.remove();
         grandColor.style.color = '#33b933'
     }
-
 })
-
 const nextbtn = document.getElementById('nextbtn');
-
 numberField.addEventListener('input', function () {
     if (numberField.value.length === 11) {
         nextbtn.disabled = false;
