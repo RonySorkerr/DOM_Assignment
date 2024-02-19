d35c4ea2d2bdd3883c51046a972f23256ffb8c36
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-19 09:39:26 +0600

    working

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const seatsLeftDisplay = document.getElementById('seatsLeft').innerText;
+
+const allBtns = document.getElementsByClassName('seat_btn');
+
+for(const eachButton of allBtns){
+    const seatNumber = eachButton.innerText;
+    
+    eachButton.addEventListener('')
+}
